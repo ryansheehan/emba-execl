f354c531b4b805253931213204a85afb15aa438f
Ryan Sheehan <rsheehan@gmail.com>
2023-05-08 10:32:07 -0500

    removing top-level await

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,39 +16,41 @@ let logout = emptyFn;
 let handleCallback = emptyFn;
 
 if(browser) {
-    const auth0:Auth0Client = await createAuth0Client({
-        domain: import.meta.env.AUTH0_DOMAIN,
-        clientId: import.meta.env.AUTH0_CLIENT_ID,
-        authorizationParams: {
-            redirect_uri: import.meta.env.AUTH0_REDIRECT_URI,        
-        },
-    }); 
-
-    login = () => {
-        console.log('login called');
-        return auth0.loginWithRedirect();
-    };
-
-    logout = async () => {
-        console.log('logout called');emptyFn
-        await auth0.logout({
-            logoutParams: {
-                returnTo: import.meta.env.AUTH0_LOGOUT_URI,
-            }
-        });
-        user_internal.set(await auth0.getUser());
-        isAuthenticated_internal.set(await auth0.isAuthenticated());
-    }
-
-    handleCallback = async () => {
-        console.log('handleCallback called');
-        const res = await auth0.handleRedirectCallback();  
-        console.log(res);
-        user_internal.set(await auth0.getUser());
-        isAuthenticated_internal.set(await auth0.isAuthenticated());   
-        goto('/');
-        console.log('finished callback', get(user));
-    }
+    (async function(){
+        const auth0:Auth0Client = await createAuth0Client({
+            domain: import.meta.env.AUTH0_DOMAIN,
+            clientId: import.meta.env.AUTH0_CLIENT_ID,
+            authorizationParams: {
+                redirect_uri: import.meta.env.AUTH0_REDIRECT_URI,        
+            },
+        }); 
+    
+        login = () => {
+            console.log('login called');
+            return auth0.loginWithRedirect();
+        };
+    
+        logout = async () => {
+            console.log('logout called');emptyFn
+            await auth0.logout({
+                logoutParams: {
+                    returnTo: import.meta.env.AUTH0_LOGOUT_URI,
+                }
+            });
+            user_internal.set(await auth0.getUser());
+            isAuthenticated_internal.set(await auth0.isAuthenticated());
+        }
+    
+        handleCallback = async () => {
+            console.log('handleCallback called');
+            const res = await auth0.handleRedirectCallback();  
+            console.log(res);
+            user_internal.set(await auth0.getUser());
+            isAuthenticated_internal.set(await auth0.isAuthenticated());   
+            goto('/');
+            console.log('finished callback', get(user));
+        }
+    })(); 
 }
 
 export {
